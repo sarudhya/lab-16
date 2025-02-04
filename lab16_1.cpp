8ebfd2f0d87f1b95d458026c8742a2a6a5e099db
sarudhya <sarudhya.achavakul@gmail.com>
2025-02-04 13:45:30 +0700

    lab 16 completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,43 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData( double *a, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a, int N , int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = rand()%101/100.0;
+    }
+}
+
+void findRowSum( const double *a,double *sum, int N, int M){
+    int k = 0;
+    
+    for(int i = 0; i < N ; i++){
+        *(sum+i) = 0;
+        for(int j = 0; j < M; j++){
+            *(sum+i) += *(a+k);
+            k++;
+        }
+        
+        
+    }
+}
+
+void findColSum( const double *a,double *sum,int N, int M){
+    int k = 0;
+    for(int i = 0; i < M; i++){
+        *(sum+i) = 0;
+        for(int j = 0; j < N; j++){
+            *(sum+i) += *(a+k+(M*j));
+        }
+        k++;
+    }
+}
